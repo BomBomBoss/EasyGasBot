5769f02aad6e12100317ef7c4c5160cfcaaca8f5
vladmih <vladislavs.mihejenkovs@citadele.lv>
2023-10-12 23:04:13 +0300

    created RepositoryFactory

---
+++ GasStationsNode/src/main/java/org/easybot/factory/RepositoryFactory.java
@@ -0,0 +1,43 @@
+package org.easybot.factory;
+
+import org.easybot.CommonTexts;
+import org.easybot.repository.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class RepositoryFactory {
+
+    private final NesteRepository nesteRepository;
+    private final CircleRepository circleRepository;
+    private final ViadaRepository viadaRepository;
+    private final VirsiRepository virsiRepository;
+    private Map<String, CommonStationRepository> repositoryMap = new HashMap<>();
+
+
+    public RepositoryFactory(NesteRepository nesteRepository, CircleRepository circleRepository, ViadaRepository viadaRepository, VirsiRepository virsiRepository)
+    {
+        this.nesteRepository = nesteRepository;
+        this.circleRepository = circleRepository;
+        this.viadaRepository = viadaRepository;
+        this.virsiRepository = virsiRepository;
+        setRepositoryMap();
+    }
+
+    private void setRepositoryMap()
+    {
+        repositoryMap.put(CommonTexts.NESTE_TITLE, nesteRepository);
+        repositoryMap.put(CommonTexts.CIRCLE_WITHOUT_K_TITLE, circleRepository);
+        repositoryMap.put(CommonTexts.VIADA_TITLE, viadaRepository);
+        repositoryMap.put(CommonTexts.VIRSI_TITLE, virsiRepository);
+    }
+
+    public Map<String, CommonStationRepository> getRepositoryMap()
+    {
+        return repositoryMap;
+    }
+}
--- GasStationsNode/src/main/java/org/easybot/service/CommonStationService.java
+++ GasStationsNode/src/main/java/org/easybot/service/CommonStationService.java
@@ -2,6 +2,7 @@ package org.easybot.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.easybot.entity.CommonStation;
+import org.easybot.factory.RepositoryFactory;
 import org.easybot.repository.*;
 import org.springframework.stereotype.Service;
 
@@ -12,50 +13,43 @@ import static org.easybot.CommonTexts.*;
 @Service
 @Slf4j
 public class CommonStationService {
-    private final NesteRepository nesteRepository;
-    private final CircleRepository circleRepository;
-    private final ViadaRepository viadaRepository;
-    private final VirsiRepository virsiRepository;
+   private final RepositoryFactory repositoryFactory;
 
-    public CommonStationService(NesteRepository nesteRepository, CircleRepository circleRepository, ViadaRepository viadaRepository, VirsiRepository virsiRepository)
+    public CommonStationService(RepositoryFactory repositoryFactory)
     {
-        this.nesteRepository = nesteRepository;
-        this.circleRepository = circleRepository;
-        this.viadaRepository = viadaRepository;
-        this.virsiRepository = virsiRepository;
+        this.repositoryFactory = repositoryFactory;
     }
+
 
     public void save(CommonStation station, String gasStationTitle)
     {
-        returnCorrectRepository(gasStationTitle).save(station);
+        getRepositoryInstance(gasStationTitle).save(station);
     }
 
     public void deleteTable(String gasStationTitle)
     {
-        returnCorrectRepository(gasStationTitle).clearTable();
+        getRepositoryInstance(gasStationTitle).clearTable();
     }
 
     public List<CommonStation> retrieveAll(String tableTitle)
     {
-      return returnCorrectRepository(tableTitle).findAll();
+      return getRepositoryInstance(tableTitle).findAll();
 
     }
 
-    private CommonStationRepository returnCorrectRepository(String title)
+    private CommonStationRepository getRepositoryInstance(String title)
     {
         if (title.equals(CIRCLE_K_TITLE))
             title = title.substring(0, title.length()-2);
 
-        return switch (title)
-                {
-                    case NESTE_TITLE -> nesteRepository;
-                    case CIRCLE_WITHOUT_K_TITLE -> circleRepository;
-                    case VIADA_TITLE -> viadaRepository;
-                    case VIRSI_TITLE -> virsiRepository;
+        CommonStationRepository repository = repositoryFactory.getRepositoryMap().get(title);
 
-                    default -> throw new RuntimeException("Can't return repository instance");
+        if(repository==null)
+        {
+            throw new RuntimeException ("Can't return repository instance");
+        }
 
-                };
+        return repository;
 
     }
 }
